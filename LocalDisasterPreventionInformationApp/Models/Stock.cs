f04051cff1ead9c6c70a30b333d783a8129c5a3e
優奈 青木 <oibc36154@ojs.ac.jp>
2026-01-15 12:12:29 +0900

    データベースのモデル完成

---
+++ LocalDisasterPreventionInformationApp/Models/Stock.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalDisasterPreventionInformationApp.Models {
+    public class Stock {
+
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }  //在庫ID
+
+        [NotNull]
+        public int ProductId { get; set; }  //商品ID
+
+        [NotNull]
+        public DateTime ExpirationDate { get; set; }  //消費期限
+
+        [NotNull]
+        public int Quantity { get; set; }  //数量
+    }
+}
