312c2e6a66d749b6e0bf620464e3bae2be449fce
優奈 青木 <oibc36154@ojs.ac.jp>
2026-01-20 11:33:22 +0900

    競合解決

--- LocalDisasterPreventionInformationApp/App.xaml.cs
+++ LocalDisasterPreventionInformationApp/App.xaml.cs
@@ -13,13 +13,20 @@ namespace LocalDisasterPreventionInformationApp {
         }
 
         protected override Window CreateWindow(IActivationState? activationState) {
-            //bool isFirstLaunch = Preferences.Get("IsFirstLaunch", true);
-            bool isFirstLaunch = true;
+            bool isFirstLaunch = Preferences.Get("IsFirstLaunch", true);
 
             //初回起動
             if (isFirstLaunch) {
                 Preferences.Set("IsFirstLaunch", false);
-                return new Window(new RegisterPage());
+
+                //初回起動時にのみDBにデータ追加
+                Task.Run(async () => {
+                    await _shelterService.FetchAndSaveShelterAsync();
+                });
+
+                var shell = new AppShell();
+                shell.GoToAsync("//RegisterPage");
+                return new Window(shell);
             } else {
                 return new Window(new AppShell());
             }
