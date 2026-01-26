c770db8f05bb4ba0c41c44b9841495b50f52bb81
優奈 青木 <oibc36154@ojs.ac.jp>
2026-01-26 15:59:00 +0900

    追加DBモデル設計まで完成

---
+++ LocalDisasterPreventionInformationApp/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalDisasterPreventionInformationApp.Models {
+
+    //ユーザ情報テーブル
+    public class User {
+
+        [PrimaryKey,AutoIncrement]
+        public int UserId { get; set; }  //ユーザID
+
+        [NotNull]
+        public string Name { get; set; }  //氏名
+
+        [NotNull]
+        public string Furigana { get; set; }  //フリガナ
+
+        [NotNull]
+        public string PhoneNumber { get; set; }  //電話番号
+
+        [NotNull]
+        public string Email { get; set; }  //メールアドレス
+    }
+}
