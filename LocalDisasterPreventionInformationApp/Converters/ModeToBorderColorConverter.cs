5655340b4344b24e93dbd0dc6f0df5f1be2d7e5b
百花 齊藤 <oibc36050@ojs.ac.jp>
2026-02-26 16:52:47 +0900

    変更やめました

---
+++ LocalDisasterPreventionInformationApp/Converters/ModeToBorderColorConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace LocalDisasterPreventionInformationApp.Converters {
+    public class ModeToBorderColorConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            string current = value as string;
+            string mode = parameter as string;
+
+            if (current == mode)
+                return Colors.Black;   // 選択中は黒枠
+
+            return Colors.Transparent; // 非選択は枠なし
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
---
+++ LocalDisasterPreventionInformationApp/Converters/ModeToColorConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace LocalDisasterPreventionInformationApp.Converters {
+    public class ModeToColorConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            string current = value as string;      // 現在のモード
+            string mode = parameter as string;     // ボタン側のモード
+
+            if (current == mode)
+                return Color.FromArgb("#8CAFA4");  // 選択中の色（濃い）
+
+            return Color.FromArgb("#DDDDDD");      // 非選択の色（薄い）
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
